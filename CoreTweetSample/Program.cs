a12312fbbd9dd4f6db4356a1bd77c581688b3b46
tkey.dll <tkey.dll@gmail.com>
2016-12-24 10:23:10 +0900

    プロジェクト ファイルを追加します。

---
+++ CoreTweetSample/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoreTweetSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var client = new Twitter();
+
+            var id = client.Tweet("hoge");
+
+            Console.WriteLine(id);
+
+            Console.WriteLine("Press Enter: ");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ CoreTweetSample/Twitter.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CoreTweet;
+using CoreTweet.Core;
+using System.Configuration;
+
+namespace CoreTweetSample
+{
+    public class Twitter
+    {
+        private string _apiKey = ConfigurationManager.AppSettings["TWITTER_API_KEY"];
+        private string _apiSecret = ConfigurationManager.AppSettings["TWITTER_API_SECRET"];
+        private string _accessToken = ConfigurationManager.AppSettings["TWITTER_ACCESS_TOKEN"];
+        private string _accessSecret = ConfigurationManager.AppSettings["TWITTER_ACCESS_SECRET"];
+
+        private Tokens _tokens;
+
+        public Twitter()
+        {
+            _tokens = CoreTweet.Tokens.Create(_apiKey, _apiSecret, _accessToken, _accessSecret);
+        }
+
+        public long Tweet(string tweetText)
+        {
+            var response = _tokens.Statuses.UpdateAsync(tweetText);
+            return response.Id;
+        }
+
+        public long Reply(string tweetText, long? replyToStatusId)
+        {
+            var response = _tokens.Statuses.UpdateAsync(status => tweetText, in_reply_to_status_id => replyToStatusId);
+            return response.Id;
+        }
+
+        public long Favorite(long tweetId)
+        {
+            var response = _tokens.Favorites.CreateAsync(tweetId);
+            return response.Id;
+        }
+    }
+}
